c6c52364ed770b976b3f97e38401c8a8bf653b49
Janne Wernecken <dev@wernecken.com>
2025-07-16 09:00:39 +0200

    fix: L2min rarity score calculation

--- icrlearn/rarity/_l2min.py
+++ icrlearn/rarity/_l2min.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 from sklearn.neighbors import NearestNeighbors
 
@@ -5,6 +6,8 @@ from sklearn.neighbors import NearestNeighbors
 def calculate_l2min(X, y, n_neighbors=5, psi=1, beta=0.5):
     if isinstance(X, pd.DataFrame):
         X = X.to_numpy()
+
+    y = np.asarray(y)
 
     nn = NearestNeighbors(
         n_neighbors=n_neighbors,
